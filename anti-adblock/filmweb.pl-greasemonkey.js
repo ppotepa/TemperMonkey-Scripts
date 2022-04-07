62baabf3c811a152edc97fe24cff7030ddb1f886
PGS-SOFT\ppotepa <ppotepa@pgs-soft.com>
2022-04-07 18:24:06 +0200

    Update Filmweb Add Wykop

--- anti-adblock/filmweb.pl-greasemonkey.js
+++ anti-adblock/filmweb.pl-greasemonkey.js
@@ -1,30 +1,25 @@
 // ==UserScript==
 // @name         FilmWeb Anti-Adblock Script
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.1
 // @description  Lazy one but does the trick
-// @author       Paweł Potępa
+// @author       You
 // @match        https://www.filmweb.pl/*
-// @web-site     https://github.com/ppotepa
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=filmweb.pl
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
-  "use strict";
-  var targetSelector = document.contains(document.querySelector("#qiqxx"));
-  var observer = new MutationObserver(function (mutations) {
-    if (document.contains(targetSelector)) {
-      targetSelector.remove();
-      observer.disconnect();
-    }
-  });
+    'use strict';
+    var targetElement = document.querySelector('#uqrqi');
+    var observer = new MutationObserver(function (mutations) {
+        if (document.contains(targetElement)) {
+            targetElement.remove();
+            observer.disconnect();
+        }
+    });
 
-  observer.observe(document, {
-    attributes: false,
-    childList: true,
-    characterData: false,
-    subtree: true,
-  });
+    observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
+
 })();
